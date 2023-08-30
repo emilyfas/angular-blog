73950e77d6e24945ad7394077789915205c99353
Emilly Fernandes <emilly.fernandesads@gmail.com>
2023-08-30 20:01:26 -0300

    adicionando componentes principais da pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PageTitleComponent } from './components/page-title/page-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PageTitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
