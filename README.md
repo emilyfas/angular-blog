778fdb951e053146552bc591a46699de642901f9
Emilly Fernandes <emilly.fernandesads@gmail.com>
2023-09-01 10:52:23 -0300

    inserindo noticias no blog e atualizando readme

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
 # AngularBlog
 
+Projeto feito com intuito de praticar conhecimentos adquiridos sobre Angular. Projeto consiste em uma página inicial com cards de notícias, além de uma página de detalhes, em que ao clicar em uma notícia na página inicial, a pagina de detalhes é chamada, e o conteúdo da notícia é chamado de forma dinâmica.
+
+## Instruções de Uso
+1. Acesse o site [Blog](https://emilyfas.github.io/angular-blog/) no seu navegador.
+2. Navegue pelas notícias exibida na página inicial.
+3. Clique em uma notícia para visualizar seus detalhes.
+
+## Tecnologias Utilizadas
+- HTML5 e CSS3
+- TypeScript
+- Angular
+
+## Autora
+O projeto foi desenvolvido por Emilly Fernandes durante. Você pode entrar em contato por:
+
+<div>
+<a href="https://wa.me/5531989018696?text=Me+mande+um+Oi+%3A%29" target="_blank"><img src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" target="_blank"></a>
+<a href="https://instagram.com/emillygarai" target="_blank"><img src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white" target="_blank"></a>
+<a href="https://twitter.com/emilly_fernads" target="_blank"><img src="https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white" target="_blank"></a>
+<a href = "mailto:emilly.fernandesads@gmail.com"><img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" target="_blank"></a>
+<a href="https://www.linkedin.com/in/emilly-fernandes" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"></a>   
+</div>
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.2.
-
-## Development server
-
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,10 +1,112 @@
 export const dataFake = [   
+
     {
         'id':'1',
-        'photo':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4JHouoAdHMIhJgZoa8lUl7cnnLEgWDIuA-g&usqp=CAU',
+        'photo':'https://queennet.com.br/wp-content/uploads/2020/05/5db21df-Queen-1-2.jpg',
+        'title':'Bohemian Rhapsody: A enigmática vida de John',
+        'description':'Antes de ganhar o famoso nome Queen, a banda se chamava Smile. O nome original foi dado apenas em 1970, quando Freddie Mercury substituiu Tim Staffel como vocalista e sugeriu a mudança. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Agosto 30, 1975'
+
+    },
+
+    {
+        'id':'2',
+        'photo':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbBVKDa47No6xk8fa_L2xxmOnl84J1POTgEA&usqp=CAU',
         'title':'Beatles alcançam marco mundial de discos vendidos',
-        'description':'A bansa sensação do momento The Beatles está conquistando o mundo a musica cada dia mais, a mais nova do grupo foi o marco de mais discos vendidos no mundo, alem de varios premios que eles ganharam este ano com seu album The Beatles, Beatles alcançam marco mundial de discos vendidos',
-        'date':'Agosto 20, 2016'
+        'description':'A bansa sensação do momento The Beatles está conquistando o mundo a musica cada dia mais, a mais nova do grupo foi o marco de mais discos vendidos no mundo, alem de varios premios que eles ganharam este ano com seu album The Beatles, Beatles alcançam marco mundial de discos vendidos. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Agosto 30, 1975'
+
+    },
+
+     {
+        'id':'3',
+        'photo':'https://veja.abril.com.br/wp-content/uploads/2017/04/belchior-cantor.jpg?quality=90&strip=info&w=1125&h=720&crop=1',
+        'title':'',
+        'description':'',
+        'date':'Junho 24, 1975'
+
+    },
+
+
+    {
+        'id':'4',
+        'photo':'https://s2-g1.glbimg.com/cBWgBr-7ggaegcbh_XPNwbUblkc=/138x0:1488x844/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/h/o/ZOOhSFRvWUnIWlC9BfvA/acdc.jpg',
+        'title':'ACDC: Com contrato assinado e com algumas mudanças',
+        'description':'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis perferendis unde quos praesentium vitae eveniet consequatur numquam molestias eos illum. Autem vero consectetur sunt corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Junho 24, 1975'
+
+    },
+
+    {
+        'id':'5',
+        'photo':'https://s2-marieclaire.glbimg.com/sMYZQ_ScvXBSpruoebAoVDLDssU=/0x0:500x389/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_51f0194726ca4cae994c33379977582d/internal_photos/bs/2023/P/q/2nu54WRESy6owY3EQqCw/2022-01-18-17032015122049elis-pedrom-c.jpeg',
+        'title':'Fascinação: Nova musica de Elis Regina acaba de sair',
+        'description':'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis perferendis unde quos praesentium vitae eveniet consequatur numquam molestias eos illum. Autem vero consectetur sunt corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Agosto 30, 1975'
+
+    },
+
+    {
+        'id':'6',
+        'photo':'https://www.rbsdirect.com.br/filestore/7/2/2/1/0/3_6636a139ecdd8e3/301227_1eeabc6247745fb.jpg?w=700',
+        'title':'Eu nasci a dez mil anos atrás',
+        'description':'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis perferendis unde quos praesentium vitae eveniet consequatur numquam molestias eos illum. Autem vero consectetur sunt corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Agosto 30, 1975'
+
+    },
+
+    {
+        'id':'7',
+        'photo':'https://akamai.sscdn.co/uploadfile/letras/fotos/3/4/1/b/341be9c93b5809ae1cf9862d71319531.jpg',
+        'title':'\'Rei do Pop\', novo titulo a Micheal Jackson',
+        'description':'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis perferendis unde quos praesentium vitae eveniet consequatur numquam molestias eos illum. Autem vero consectetur sunt corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Agosto 30, 1975'
+
+    },
+
+    {
+        'id':'8',
+        'photo':'https://media.vogue.co.uk/photos/5d54c464999dc600085243f7/16:9/w_2580,c_limit/original',
+        'title':'Considerada uma das figuras de maior impacto na cultura popular',
+        'description':'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis perferendis unde quos praesentium vitae eveniet consequatur numquam molestias eos illum. Autem vero consectetur sunt corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Agosto 30, 1975'
+
+    },
+
+    {
+        'id':'9',
+        'photo':'https://s2-g1.glbimg.com/I_LnxeKoV2frTcFPQYeFuHb-Rps=/0x0:1200x986/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/W/G/MAb85wSKCaxukABGlSAg/legiao.jpg',
+        'title':'Banda brasileira de rock formada em 1982, em Brasília',
+        'description':'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis perferendis unde quos praesentium vitae eveniet consequatur numquam molestias eos illum. Autem vero consectetur sunt corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Agosto 30, 1975'
+
+    },
+
+    {
+        'id':'10',
+        'photo':'https://www.osparalamas.com.br/wp-content/uploads/2020/05/bio3.jpg',
+        'title':'20 músicas dos Paralamas, de autoria de Herbert Vianna',
+        'description':'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis perferendis unde quos praesentium vitae eveniet consequatur numquam molestias eos illum. Autem vero consectetur sunt corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Agosto 30, 1975'
+
+    },
+
+
+    {
+        'id':'11',
+        'photo':'https://i.pinimg.com/originals/3a/74/9c/3a749cbfae1da7eb14fad5210556c52c.png',
+        'title':'Capital Inicial leva turnê comemorativa para Belo Horizonte',
+        'description':'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis perferendis unde quos praesentium vitae eveniet consequatur numquam molestias eos illum. Autem vero consectetur sunt corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Agosto 30, 1975'
+
+    },
+
+    {
+        'id':'12',
+        'photo':'https://akamai.sscdn.co/tb/letras-blog/wp-content/uploads/2021/09/7325676-engenheiros-1024x795.jpg',
+        'title':'\'Engenheiros do Hawaii era um nome desmistificador, ninguém nos levaria muito a sério\'',
+        'description':'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis perferendis unde quos praesentium vitae eveniet consequatur numquam molestias eos illum. Autem vero consectetur sunt corrupti. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora temporibus illo architecto aperiam officia nulla optio delectus ea minus, quibusdam, ad aut sapiente debitis magnam officiis beatae. Numquam, inventore Quidem nulla odio possimus labore nesciunt aliquam sunt, praesentium ipsa aliquid quo, quasi veniam sint quis adipisci doloribus unde ut neque, deleniti libero a. Possimus iusto quaerat laborum alias deserunt.Voluptatem porro voluptate aliquam, veritatis impedit a vitae beatae architecto distinctio amet necessitatibus quibusdam atque reiciendis labore sunt velit ex hic, dolorum tenetur doloremque exercitationem quo fugit. Ut, dolor sequi.Perferendis obcaecati unde atque doloremque, totam ipsum! Fuga deleniti dolores fugit. Aperiam molestias ipsam, non ducimus veniam ex, deleniti quae, delectus dolor ad quibusdam? Qui dignissimos magnam corrupti expedita voluptas.Quas, pariatur maiores fugiat ipsa consequuntur cupiditate dicta fuga deserunt delectus aspernatur magni? Deleniti, consequatur expedita! Ipsam sapiente quasi iusto laudantium inventore tempore, eum non, sint cum ad, perspiciatis praesentium.',
+        'date':'Agosto 30, 1975'
 
     }
 ]
